d35b4be758e63d6661215dc1afb44515a03aee22
Xiong XinJiang <630774117@qq.com>
2016-09-07 11:18:56 +0800

    test first commit

---
+++ src/main/java/cn/jninber/testlib/HelloClass.java
@@ -0,0 +1,12 @@
+package cn.jninber.testlib;
+
+/**
+ * Created by jninber on 16/9/7.
+ */
+
+public class HelloClass {
+
+    public static String getTestString() {
+        return "这是测试文字";
+    }
+}
